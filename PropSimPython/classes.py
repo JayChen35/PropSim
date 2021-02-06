79d4ace16d9627b8940d9a3a2f07157eb0bc5578
JayChen35 <jay.chen135@gmail.com>
2021-02-06 16:28:38 -0500

    Add Struct class and progress on n2o_properties

--- PropSimPython/classes.py
+++ PropSimPython/classes.py
@@ -1,21 +1,23 @@
-# PropSim Classes
+# PropSimPython Classes
 # Project Caelus, Aphlex 1C Engine
 # 04 February, 2021
 
 
-class Inputs():
+class Struct():
+    """
+    Generic storage of variables class.
+    """
+    def __init__(self):
+        super().__init__()
+
+
+class Inputs(Struct):
     """
     Stores inputs for PropSim
     """
     def __init__(self):
         super().__init__()
 
-        self.ox = None
-        self.ox_pressurant = None
-        
-        self.fuel = None
-        self.fuel_pressurant = None
-        
         self.dt_valve_open = None
         self.mass_dry_rocket = None
         
@@ -39,22 +41,31 @@ class Inputs():
         self.ox_pressurant = Pressurant()
         self.fuel_pressurant = Pressurant()
         
+        self.ox.injector_area = None
+        self.ox.Cd_injector = None
+        self.ox.V_l = None
+        self.ox.V_tank = None
+        self.ox.tank_id = None
+        self.ox.h_offset_tank = None
+        self.ox.d_flowline = None
+        self.ox.T_tank = None
+
+        self.fuel.injector_area = None
+        self.fuel.Cd_injector = None
+        self.fuel.h_offset_tank = None
+        self.fuel.d_flowline = None
+        self.fuel.rho = None
+
         # ox: injector_area, Cd_injector , V_l, V_tank, tank_id, h_offset_tank, d_flowline, T_tank
         # fuel: injector_area, Cd_injector, h_offset_tank, d_flowline, rho
-        # dt_valve_open
-        # mass_dry_rocket
-        # d_cc
-        # length_cc
+
+        self.dt_valve_open
+        self.mass_dry_rocket
+        self.d_cc
+        self.length_cc
         
         # ox_pressurant: gas_properties, set_pressure, storage_initial_pressure, tank_volume, flow_CdA
         # fuel_pressurant: gas_properties, set_pressure, storage_initial_pressure, tank_volume, flow_CdA
-
-class Struct():
-    """
-    Generic storage of variables class.
-    """
-    def __init__(self):
-        super().__init__()
 
 class Gas():
     """
--- PropSimPython/prop_sim.py
+++ PropSimPython/prop_sim.py
@@ -3,8 +3,8 @@
 
 
 import numpy as np 
-from classes import Gas, Inputs, Pressurant
-from helpers import isen_exp
+from classes import Gas, Inputs, Pressurant, Struct
+from helpers import n2o_properties
 
 
 #----------Unit Conversions-------------
@@ -22,6 +22,14 @@ nitrogen.c_v = 0.743e3 # J/kg*K
 nitrogen.molecular_mass = 2*14.0067e-3 # kg/mol
 
 inputs = Inputs()
+options = Struct()
+options.t_final = 60 # sec
+options.dt = 0.01 # sec
+options.output_on = True
+# t_final: simulation time limit
+# dt: output time resolution
+# output_on: true for plots, false for no plots
+
 #-------Injector Properties----------
 # Injector Exit Area
 inputs.ox.injector_area = 2.571e-05 # m^2
@@ -39,22 +47,22 @@ inputs.fuel.Cd_injector = 0.88
 inputs.mass_dry_rocket = 30*lbm_to_kg
 
 #-------Oxidizer Properties--------
-#Tank Volume
+# Tank Volume
 inputs.ox.V_tank = 11.564*L_to_m3
 
-#Nitrous Volume
+# Nitrous Volume
 inputs.ox.V_l = 4.208*L_to_m3 # 3.59
 
-#Tank Inner Diameter
+# Tank Inner Diameter
 inputs.ox.tank_id = 5*in_to_m
 
-#Distance from Bottom of Tank to Injector
+# Distance from Bottom of Tank to Injector
 inputs.ox.h_offset_tank = 2 # 0 m
 
-#Main Flow Line Diameter
+# Main Flow Line Diameter
 inputs.ox.d_flowline = .5*in_to_m
 
-#Tank Temperature (K)
+# Tank Temperature (K)
 inputs.ox.T_tank = 292
 
 #-------Oxidizer Pressurant Properties--------
@@ -68,19 +76,19 @@ inputs.ox.T_tank = 292
 
 #-------Fuel Properties--------
 
-#Tank Volume
+# Tank Volume
 inputs.fuel.V_tank = 11.564*L_to_m3
 
-#Fuel Volume
+# Fuel Volume
 inputs.fuel.V_l = 1.267*L_to_m3 # 0.881
 
-#Tank Inner Diameter
+# Tank Inner Diameter
 inputs.fuel.tank_id = 5*in_to_m
 
-#Distance from Bottom of Tank to Injector
+# Distance from Bottom of Tank to Injector
 inputs.fuel.h_offset_tank = 24*in_to_m # 24*in_to_m
 
-#Main Flow Line Diameter(in)
+# Main Flow Line Diameter(in)
 inputs.fuel.d_flowline = .5*in_to_m
 
 inputs.fuel.rho = 789 # Kg/m^3
@@ -96,36 +104,37 @@ inputs.fuel_pressurant.flow_CdA = 8*mm_to_m^2
 
 #-------Other Properties--------
 
-#Combustion chamber dimensions
+# Combustion chamber dimensions
 inputs.length_cc = 0.258 # m
 inputs.d_cc = 0.08 # m
 
-#Estimated nozzle efficiency
+# Estimated nozzle efficiency
 inputs.nozzle_efficiency = 0.95
 inputs.nozzle_correction_factor = 0.9830
 
 # Estimated combustion efficiency
 inputs.c_star_efficiency = 0.85
 
-#Nozzle Throat diameter
+# Nozzle Throat diameter
 inputs.d_throat = 30.46e-3
 
-#Expansion Ratio
+# Expansion Ratio
 inputs.exp_ratio = 3.26
 
-#Ambient Temperature
+# Ambient Temperature
 inputs.T_amb = 292 # K
 
-#Ambient Pressure
+# Ambient Pressure
 inputs.p_amb = 9.554e04 # Pa
 
 # Load Combustion Data
 # inputs.comb_data = load(inputs.CombustionData) 
 # inputs.comb_data = inputs.comb_data.CombData
 
-# Run Performance Code
-PerformanceCode(inputs, mode, test_data)
-
 
 if __name__=="___main__":
-    pass
+    PerformanceCode(inputs, options)
+
+
+def PerformanceCode(inputs: Inputs, options: Struct):
+    n2o = n2o_properties(inputs.T_amb)
